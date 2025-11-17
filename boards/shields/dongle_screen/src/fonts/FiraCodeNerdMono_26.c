e28b36fa091b840c3e5a21fa6bf517ead60b4c7e
dakhar <beldimon@gmail.com>
2025-11-18 02:05:52 +0300

    lv_font_montserrat_32;

--- boards/shields/dongle_screen/src/fonts/FiraCodeNerdMono_26.c
+++ boards/shields/dongle_screen/src/fonts/FiraCodeNerdMono_26.c
@@ -3407,7 +3407,7 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 
 };
 
-extern const lv_font_t lv_font_montserrat_24;
+extern const lv_font_t lv_font_montserrat_32;
 
 
 /*-----------------
@@ -3433,7 +3433,7 @@ lv_font_t FiraCodeNerdMono_26 = {
 #endif
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
-    .fallback = &lv_font_montserrat_24,
+    .fallback = &lv_font_montserrat_32,
 #endif
     .user_data = NULL,
 };
