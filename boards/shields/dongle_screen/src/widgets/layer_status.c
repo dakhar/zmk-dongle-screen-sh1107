f7d68ebfcc27ec514181a920b71423580ff8bb15
dakhar <beldimon@gmail.com>
2025-11-17 19:59:53 +0300

    add inc

--- boards/shields/dongle_screen/src/widgets/layer_status.c
+++ boards/shields/dongle_screen/src/widgets/layer_status.c
@@ -13,8 +13,9 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/event_manager.h>
 #include <zmk/endpoints.h>
 #include <zmk/keymap.h>
-
 #include <util.h>
+
+#include "layer_status.h"
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
