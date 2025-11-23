0f6b121122c216ce8a882e1696bc7b5db36c25f1
dakhar <beldimon@gmail.com>
2025-11-23 13:56:33 +0300

    resolution

--- boards/shields/dongle_screen/include/util.h
+++ boards/shields/dongle_screen/include/util.h
@@ -2,14 +2,14 @@
 #include <lvgl.h>
 #include <zmk/endpoints.h>
 
-#define LVGL_BACKGROUND                                                                            \
+#define LVGL_BACKGROUND \
     IS_ENABLED(CONFIG_ZMK_DISPLAY_INVERT) ? lv_color_black() : lv_color_white()
-#define LVGL_FOREGROUND                                                                            \
+#define LVGL_FOREGROUND \
     IS_ENABLED(CONFIG_ZMK_DISPLAY_INVERT) ? lv_color_white() : lv_color_black()
 
-#define DISPLAY_NODE DT_CHOSEN(zephyr_display)
-#define DISPLAY_WIDTH  DT_PROP(DISPLAY_NODE, width)
-#define DISPLAY_HEIGHT DT_PROP(DISPLAY_NODE, height)
+#define DISPLAY_NODE    DT_CHOSEN(zephyr_display)
+#define DISPLAY_WIDTH   DT_PROP(DISPLAY_NODE, width)
+#define DISPLAY_HEIGHT  DT_PROP(DISPLAY_NODE, height)
 
 #if CONFIG_LV_COLOR_DEPTH_1 == 1
 #define BYTES_PER_PIXEL 1
