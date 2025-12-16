f3d516ff9f5917838247ad5c6796f52409ee1595
dakhar <beldimon@gmail.com>
2025-12-16 13:46:27 +0300

    -- lv_canvas_draw_text

--- boards/shields/dongle_screen/src/widgets/battery_status.c
+++ boards/shields/dongle_screen/src/widgets/battery_status.c
@@ -168,14 +168,24 @@ static void draw_battery(struct battery_state state, struct battery_object batte
     const int meter_width = LV_CLAMP(0, (nrg_meter_w * state.level + 50) / 100, nrg_meter_w);
     const int meter_height = LV_CLAMP(0, (nrg_meter_h * state.level + 50) / 100, nrg_meter_h);
 #ifdef CONFIG_DONGLE_SCREEN_BATTERY_VERTICAL
-    lv_canvas_draw_text (battery.canvas, battery_w, text_y, lable_max_w, &label_dsc, level_str);
+    lv_obj_t *label = lv_label_create(battery.canvas);
+    lv_label_set_text(label, level_str);
+    lv_obj_set_style_text_color(label, text_color, 0);
+    lv_obj_set_pos(label, battery_w - lable_max_w, text_y);  // Прижимаем вправо
+    lv_obj_set_size(label, lable_max_w, label_h);
+
     if (state.level < 1 || state.level > 100) return;
     lv_canvas_draw_rect(battery.canvas, 0, 0, battery_w, CONTACT_L, &rect_contact);
     lv_canvas_draw_rect(battery.canvas, 0, CONTACT_L, battery_w, battery_h, &rect_shell);
     lv_canvas_draw_rect(battery.canvas, BORDER_SZ, CONTACT_L + battery_h - BORDER_SZ - meter_height, 
                         nrg_meter_w, meter_height, &rect_meter);
 #else
-    lv_canvas_draw_text (battery.canvas, 0, 0, lable_max_w, &label_dsc, level_str); 
+    lv_obj_t *label = lv_label_create(battery.canvas);
+    lv_label_set_text(label, level_str);
+    lv_obj_set_style_text_color(label, text_color, 0);
+    lv_obj_set_pos(label, 0, 0);
+    lv_obj_set_size(label, lable_max_w, label_h);
+    
     if (state.level < 1 || state.level > 100) return;
     if (battery_h < 3) return;
     lv_canvas_draw_rect(battery.canvas, 0, label_h, CONTACT_L, battery_h, &rect_contact);
